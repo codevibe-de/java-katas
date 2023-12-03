1a4378b2672b2473cc279b4c95f4c042637fe8b2
Thomas Auinger <thomas@auinger.de>
2023-12-03 20:26:17 +0100

    without sltn

--- src/main/java/datetime/DateTimeLogic.java
+++ src/main/java/datetime/DateTimeLogic.java
@@ -5,30 +5,22 @@ import java.time.*;
 public class DateTimeLogic {
 
     public LocalDate createDateForThisYearsXmas() {
-        return LocalDate.now().withMonth(Month.DECEMBER.getValue()).withDayOfMonth(24);
+        return null;
     }
 
     public ZonedDateTime convertToSydneyZone(ZonedDateTime input) {
-        return input.withZoneSameInstant(ZoneId.of("Australia/Sydney"));
+        return null;
     }
 
     public LocalDate parseAndPostponeForTwoWeeks(String input) {
-        var date = LocalDate.parse(input);
-        return date.plusWeeks(2);
+        return null;
     }
 
     public String printIntervalBetweenInstantsInEnglishNotation(LocalDateTime input1, LocalDateTime input2) {
-        var duration = Duration.between(input1, input2);
-        var hours = duration.toHours();
-        duration = duration.minusHours(hours);
-        var minutes = duration.toMinutes();
-        duration = duration.minusMinutes(minutes);
-        return String.format("%dh %d' %d\"",
-                hours, minutes, duration.toSeconds());
+        return null;
     }
 
     public int calculateDaysInMonth(int year, int month) {
-        YearMonth yearMonthObject = YearMonth.of(year, month);
-        return yearMonthObject.lengthOfMonth();
+        return null;
     }
 }
