03bf43db241627458637ee2ee62e1fa735eee6ca
Thomas Auinger <thomas@auinger.de>
2023-12-04 10:13:08 +0100

    added Gradle support

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+group = "de.codevibe"
+version = "1.0-SNAPSHOT"
+description = "java-katas"
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
