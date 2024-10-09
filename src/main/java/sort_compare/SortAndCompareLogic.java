9f645396560e6bffbac15357ecc75c0ef8555eb6
Thomas Auinger <thomas@auinger.de>
2024-10-09 07:35:24 +0200

    sort kata sltn removal

--- src/main/java/sort_compare/SortAndCompareLogic.java
+++ src/main/java/sort_compare/SortAndCompareLogic.java
@@ -8,30 +8,16 @@ import java.util.List;
 public class SortAndCompareLogic {
 
     public List<Song> sortInNaturalOrder(List<Song> songs) {
-        var sorted = new ArrayList<>(songs);
-        // any of these 3 lines will work:
-        sorted.sort(null);
-        sorted.sort(Comparator.naturalOrder());
-        Collections.sort(sorted);
-        return sorted;
+        return null;
     }
 
 
     public List<Song> sortByArtist(List<Song> songs) {
-        var sorted = new ArrayList<>(songs);
-        // any of these 2 lines will work:
-        sorted.sort((s1, s2) -> s1.artist().compareTo(s2.artist()));
-        sorted.sort(Comparator.comparing(Song::artist));
-        return sorted;
+        return null;
     }
 
 
     public List<Song> sortByIsrcCountryCodeThenYear(List<Song> songs) {
-        var sorted = new ArrayList<>(songs);
-        sorted.sort(Comparator
-                .comparing((Song s) -> s.isrc().substring(0, 2))
-                .thenComparing(Song::year)
-        );
-        return sorted;
+        return null;
     }
 }
